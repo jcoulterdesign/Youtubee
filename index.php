1f6e964cc0320477ab60cfb31706f09d620f8339
McDonald95 <reecemcdonald40@gmail.com>
2014-11-25 11:18:37 +0000

    All Core Folders And Files Added
    Necessary files to run the plugin, Still require programming

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Youtubee
+* Plugin URI: http://www.cubidesigns.co.uk
+* Description: Adds youtube videos anywhere on your site using the shortcode:
+* Version: 1.0
+* Author: Reece McDonald
+* Author URI: http://www.cubidesigns.co.uk
+*/
