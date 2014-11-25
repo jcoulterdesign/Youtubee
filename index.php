8364b9fc5ff3000553a8399bbd0413c7b7370fa9
McDonald95 <reecemcdonald40@gmail.com>
2014-11-25 21:48:29 +0000

    Added option page function for WordPress
    code added now shows the youtubee option page within the WordPress admin
    panel, for users with the correct privelages, Found under
    settings>>Youtubee

--- index.php
+++ index.php
@@ -7,14 +7,30 @@
 * Author: Reece McDonald
 * Author URI: http://www.cubidesigns.co.uk
 */
-function youtubee_scripts()
+function youtubee_scripts() //function to call all the scripts and stylesheets required by the plugin
 {
-	wp_enqueue_style('youtubee.css', '/wp-content/plugins/Youtubee/css/youtubee.css');
+	wp_enqueue_style('youtubee.css', '/wp-content/plugins/Youtubee/css/youtubee.css'); //enqueue's stylesheet for Youtubee
 }
-function youtubee_addin()
+function youtubee_admin_menu() //function to add option page to wordpress
 {
-	
-	
+	add_options_page(
+		'Youtubee Options', 	//Page Title
+		'Youtubee',				//Menu Title
+		'manage_options',		//Capability based on roles of current user
+		'youtubee_options',		//Plugin Slug
+		'youtubee_admin_page'	//Function which outputs user options
+	);
 }
-add_action('wp_enqueue_scripts', 'youtubee_scripts');
-add_shortcode('youtubee','youtubee_addin');
+function youtubee_admin_page() //function used to output on option page seen in function above youtubee_admin_menu
+{
+	if (!current_user_can( 'manage_options'))
+	{
+		wp_die(__('You do not have sufficient permissions to access this page.')); //If the current user doesn't have sufficient admin access display this message
+	}
+	//The following is displayed when the Youtubee Options page is accessed, "Hello World!" has been outputted for testing purposes
+	echo '<div class="youtubee_wrapper">'; 
+	echo '<p>Hello World!</p>';
+	echo '</div>';
+}
+add_action('admin_menu','youtubee_admin_menu'); // Action adds menu option to wordpress through settings>>Youtubee outputting from function youtubee_admin_menu
+add_action('wp_enqueue_scripts', 'youtubee_scripts'); // Actions allows wordpress to access function youtubee_scripts to import require stylesheets and scripts
